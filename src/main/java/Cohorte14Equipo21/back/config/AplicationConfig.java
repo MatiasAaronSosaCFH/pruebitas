5c0691b389ebbfab806c10cdd9684d8cb0c00810
Matias Aaron Sosa <126860303+MatiasAaronSosaCFH@users.noreply.github.com>
2023-10-28 02:27:13 -0300

    listox3

--- src/main/java/Cohorte14Equipo21/back/config/AplicationConfig.java
+++ src/main/java/Cohorte14Equipo21/back/config/AplicationConfig.java
@@ -53,7 +53,7 @@ public class AplicationConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);

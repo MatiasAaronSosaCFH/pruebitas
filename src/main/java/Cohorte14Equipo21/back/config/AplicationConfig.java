37230abd2d4dd0f9b26517dcb4526774c39db008
Matias Aaron Sosa <126860303+MatiasAaronSosaCFH@users.noreply.github.com>
2023-10-28 01:39:53 -0300

    agregando configCors

--- src/main/java/Cohorte14Equipo21/back/config/AplicationConfig.java
+++ src/main/java/Cohorte14Equipo21/back/config/AplicationConfig.java
@@ -43,4 +43,18 @@ public class AplicationConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
 }
